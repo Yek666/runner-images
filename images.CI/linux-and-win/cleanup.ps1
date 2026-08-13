f7001c5058aadb7c1ec4ebec6f016efa9e60a6c0
Jose Eduardo Flores Balbuena <186737245+Yek666@users.noreply.github.com>
2026-08-12 20:57:50 -0600

    Actualizar cleanup.ps1

--- images.CI/linux-and-win/cleanup.ps1
+++ images.CI/linux-and-win/cleanup.ps1
@@ -1,4 +1,15 @@
-param(
+param(curl --request POST \
+  --url https://eth-mainnet.g.alchemy.com/v2/M-ssn0-kv2cTdaNk_wRyf \
+  --header 'Content-Type: application/json' \
+  --data '{
+  "jsonrpc": "2.0",
+  "id": 1,
+  "method": "eth_getBalance",
+  "params": [
+    "0x6055Dc6Ff1077eebe5e6D2BA1a1f53d7Ef8430dE",
+    "latest"
+  ]
+}'
     [Parameter (Mandatory=$true)] [string] $TempResourceGroupName
 )
 
